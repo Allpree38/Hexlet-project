7c1812218291e9c84c59cfa3221b7340df6a7f7d
Allpree38 <aapredkov@gmail.com>
2026-04-08 22:43:52 +0300

    Обновления блока с ФИО
    Добавлена минимальная разметка для теста и логика на JS

--- JS/HI.js
+++ JS/HI.js
@@ -1,3 +1,24 @@
+//Кнопка перехода на другую страницу
+
 document.getElementById('Hi_btn').onclick = ()=>{
     window.location.href = 'index.html'
+}
+
+//Вставление картинки в "рамку"
+
+function previewYourPhoto() {
+    const file = document.querySelector('input[type=file]').files[0]
+    const photo = document.getElementById('previewImg')
+    
+    if (file) {
+        const reader = new FileReader()
+        reader.onload = function(e) {
+            photo.src = e.target.result
+            photo.style.display = 'block'
+        }
+        reader.readAsDataURL(file)
+    } else {
+        photo.src = ''
+        photo.style.display = 'none'
+    }
 }
